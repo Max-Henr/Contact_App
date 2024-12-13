59719db507e74408920282f37fd06fa9960db241
Max Henriksson <maxipod.henriksson@gmail.com>
2024-12-13 10:00:17 +0100

    Add project files.

---
+++ Business/Services/FileService.cs
@@ -0,0 +1,34 @@
+﻿using Business.Interfaces;
+
+namespace Business.Services;
+
+public class FileService : IFileService
+{
+
+    private readonly string _filePath;
+    private readonly string _directoryPath;
+
+    public FileService(string fileName = "Contacts.json", string directoryPath = "Data")
+    {
+        _directoryPath = directoryPath;
+        _filePath = Path.Combine(_directoryPath, fileName);
+    }
+
+    public bool SaveContactToList()
+    {
+        try
+        {
+            if(!Directory.Exists(_directoryPath))
+                Directory.CreateDirectory(_directoryPath);
+        }
+        catch 
+        {
+
+        }
+    }
+    public string GetContactList()
+    {
+        throw new NotImplementedException();
+    }
+
+}
